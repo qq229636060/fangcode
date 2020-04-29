5edb503eef5afbdb75c9519f530913cec837e225
Le Zheng <229636060@qq.com>
2020-04-29 14:01:12 +0800

    zll

--- pages/tool/map.js
+++ pages/tool/map.js
@@ -1,6 +1,7 @@
 // pages/tool/map.js
 var QQMapWX = require('../../utils/qqmap-wx-jssdk.min.js');
 var qqmapsdk;
+var mapdata;
 Page({
 
   /**
@@ -9,7 +10,9 @@ Page({
   data: {
     markers: [],
     lng:"",
-    lat:""
+    lat:"",
+    act:"",
+    seachname:""
   },
 
   /**
@@ -20,13 +23,13 @@ Page({
      qqmapsdk = new QQMapWX({
       key: 'QAQBZ-2EBCP-4O4DZ-VFMQJ-2C6GZ-OABAD'
       });
-     var mapdata = [{
-      iconPath: "../../img/map.png",
+      mapdata = [{
+      iconPath: "../../img/map1.png",
       id: 0,
       latitude: options.lat,
       longitude: options.lng,
-      width: 50,
-      height: 50,
+      width: 20,
+      height:39,
       title:"本案地址"
     }]
     this.setData({
@@ -35,28 +38,54 @@ Page({
       lat:options.lat
     })
   },
-
+  select:function(e){
+     this.setData({
+       act:e.currentTarget.dataset.id,
+       seachname:e.currentTarget.dataset.name
+     });
+     this.getdata();
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  getdata:function(){
+    var _this = this;
     var loc = {
       latitude: this.data.lat,
       longitude: this.data.lng
     }
+    mapdata = [{
+      iconPath: "../../img/map1.png",
+      id: 0,
+      latitude: this.data.lat,
+      longitude: this.data.lng,
+      width: 20,
+      height:39,
+      title:"本案地址"
+    }]
     qqmapsdk.search({
-          keyword: '酒店',
+          keyword: _this.data.seachname,
           location: loc,
           success: function (res) {
-              this.setData({
-
+             console.log(res);
+              if(res.data.length != 0){
+                  res.data.forEach(function(item,index){
+                    mapdata.push({
+                      iconPath: "../../img/map.png",
+                      id: item.id,
+                      latitude: item.location.lat,
+                      longitude: item.location.lng,
+                      title:item.category,
+                      width:20,
+                      height:27
+                    })
+                  })
+              }
+              _this.setData({
+                markers:mapdata
               })
           },
           fail: function (res) {
@@ -66,6 +95,12 @@ Page({
           //     console.log(res);
           // }
     });
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
   },
 
   /**
